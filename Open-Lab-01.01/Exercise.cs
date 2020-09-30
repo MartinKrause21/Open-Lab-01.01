b13983fb2ea2a97949f535371a2d5ce6cbd0ca60
MartinKrause21 <martin.21.krause@gmail.com>
2020-09-30 09:13:27 +0200

    test 2

--- Open-Lab-01.01/Exercise.cs
+++ Open-Lab-01.01/Exercise.cs
@@ -6,7 +6,19 @@ namespace Open_Lab_01._01
     {
         public bool IsLessThan10(string input)
         {
-            throw new NotImplementedException();//;ljk
+            int cislo = Int32.Parse(input);
+            if (cislo < 10)
+            {
+                return true; 
+            }
+            else
+            {
+                return false;
+            }
+
+
+
+
         }
     }
 }
